85c6061a550ca9e2fbcec6f45ec8f9d1e2538930
hrishiko <hrishiko@gmail.com>
2016-01-14 14:26:03 +0530

    GPS Coordinate Added to Model

---
+++ src/com/couragedigital/petapp/model/GPSCoordinates.java
@@ -0,0 +1,29 @@
+package com.couragedigital.petapp.model;
+
+/**
+ * Created by courage digital on 1/9/2016.
+ */
+public class GPSCoordinates {
+
+
+
+    private String latitude;
+
+    private String longitude;
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+}

f5137ff97ceb86bf4574cedd725fd167480168f0
pratikcouragedigital <pratiksonawane@couragedigital.com>
2016-02-15 10:52:37 +0530

    changes in petlistDetails n petmateDetails layout chnge

--- src/com/couragedigital/petapp/PetMateListDetails.java
+++ src/com/couragedigital/petapp/PetMateListDetails.java
@@ -29,11 +29,12 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
     String secondImagePath = "";
     String thirdImagePath = "";
     String breed = "";
+    String age = "";
     String ageInMonthForPetMate = "";
     String ageInYearForPetMate = "";
     String gender = "";
     String description = "";
-    String email = "";
+    //String email = "";
     String mobileno = "";
 
     TextView petMateDetailsImageText;
@@ -43,6 +44,7 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
     ImageView petMateDetailsThirdImageThumbnail;
     ImageView petMateImage;
     TextView petMateBreed;
+    TextView petMateAge;
     TextView petMateAgeInMonth;
     TextView petMateAgeInYear;
     TextView petMateGender;
@@ -71,11 +73,12 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
             secondImagePath = intent.getStringExtra("PET_SECOND_IMAGE");
             thirdImagePath = intent.getStringExtra("PET_THIRD_IMAGE");
             breed = intent.getStringExtra("PET_MATE_BREED");
+            //age = intent.getStringExtra("PET_MATE_AGE");
             ageInMonthForPetMate = intent.getStringExtra("PET_MATE_AGE_INMONTH");
             ageInYearForPetMate = intent.getStringExtra("PET_MATE_AGE_INYEAR");
             gender = intent.getStringExtra("PET_MATE_GENDER");
             description = intent.getStringExtra("PET_MATE_DESCRIPTION");
-            email = intent.getStringExtra("POST_OWNER_EMAIL");
+            //email = intent.getStringExtra("POST_OWNER_EMAIL");
             mobileno = intent.getStringExtra("POST_OWNER_MOBILENO");
         }
 
@@ -105,23 +108,25 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
         petMateDetailsThirdImageThumbnail = (ImageView) findViewById(R.id.petMateDetailsThirdImageThumbnail);
 
         petMateBreed = (TextView) findViewById(R.id.petMateBreedInPetDetails);
+        //petListingType = (TextView) findViewById(R.id.petListingTypeInPetDetails);
+        //petMateAge = (TextView) findViewById(R.id.petMateAgeInPetDetails);
         petMateAgeInMonth = (TextView) findViewById(R.id.petMateAgeInMonthInPetDetails);
         petMateAgeInYear = (TextView) findViewById(R.id.petMateAgeInYearInPetDetails);
         petMateGender = (TextView) findViewById(R.id.petMateGenderInPetDetails);
         petMateDescription = (TextView) findViewById(R.id.petMateDescriptionInPetDetails);
         petMateDetailsDividerLine = findViewById(R.id.petMateDetailsDividerLine);
         petMateDetailsCallButton = (Button) findViewById(R.id.petMateDetailsCallButton);
-        petMateDetailsEmailButton = (Button) findViewById(R.id.petMateDetailsEmailButton);
+        //petMateDetailsEmailButton = (Button) findViewById(R.id.petMateDetailsEmailButton);
 
         petMateDetailsFirstImageThumbnail.setOnClickListener(this);
         petMateDetailsSecondImageThumbnail.setOnClickListener(this);
         petMateDetailsThirdImageThumbnail.setOnClickListener(this);
         petMateDetailsCallButton.setOnClickListener(this);
-        petMateDetailsEmailButton.setOnClickListener(this);
+      //  petMateDetailsEmailButton.setOnClickListener(this);
 
         petMateDetailsbitmap = getBitmapImageFromURL(firstImagePath);
         petMateImage.setImageBitmap(petMateDetailsbitmap);
-        petMateDetailsCollapsingToolbar.setTitle("For Mating");
+        petMateDetailsCollapsingToolbar.setTitle(breed);
 
         petMateDetailsImageText.setText("Images of " + breed);
         petMateDetailsImagesDividerLine.setBackgroundResource(R.color.list_internal_divider);
@@ -134,17 +139,17 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
             petMateDetailsbitmap = getBitmapImageFromURL(thirdImagePath);
             petMateDetailsThirdImageThumbnail.setImageBitmap(petMateDetailsbitmap);
         }
-        String breedOfPet = "<b>Breed: </b>" + breed;
+        String breedOfPet ="<b>Breed: </b>"+ breed;
         petMateBreed.setText(Html.fromHtml(breedOfPet));
         //String ageOfPet = "<b>Age: </b>" + age;
         //petMateAge.setText(Html.fromHtml(ageOfPet));
-        String ageOfPetInMonth = "<b>Month: </b>" + ageInMonthForPetMate;
+        String ageOfPetInMonth = "<b>Months: </b>"+ageInMonthForPetMate;
         petMateAgeInMonth.setText(Html.fromHtml(ageOfPetInMonth));
-        String ageOfPetInYear = "<b>Year: </b>" + ageInYearForPetMate;
+        String ageOfPetInYear = "<b>Years: </b>"+ageInYearForPetMate;
         petMateAgeInYear.setText(Html.fromHtml(ageOfPetInYear));
-        String genderOfPet = "<b>Gender: </b>" + gender;
+        String genderOfPet =  gender;
         petMateGender.setText(Html.fromHtml(genderOfPet));
-        String descriptionOfPet = "<b>Description: </b>" + description;
+        String descriptionOfPet =  description;
         petMateDescription.setText(Html.fromHtml(descriptionOfPet));
         petMateDetailsDividerLine.setBackgroundResource(R.color.list_internal_divider);
 
@@ -218,7 +223,7 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
             callIntent.setData(Uri.parse("tel:"+mobileno));
             startActivity(callIntent);
         }
-        else if(v.getId() == R.id.petMateDetailsEmailButton) {
+        /*else if(v.getId() == R.id.petMateDetailsEmailButton) {
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                     "mailto",email, null));
             startActivity(Intent.createChooser(emailIntent, "Choose an Email client :"));
@@ -234,6 +239,6 @@ public class PetMateListDetails extends AppCompatActivity implements View.OnClic
         else if(v.getId() == R.id.petMateDetailsThirdImageThumbnail) {
             petMateDetailsbitmap = getBitmapImageFromURL(thirdImagePath);
             petMateImage.setImageBitmap(petMateDetailsbitmap);
-        }
+        }*/
     }
 }
